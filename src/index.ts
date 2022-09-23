de5641fbb216c14a8be2ef26001f723e2e9e9f9d
Lenny Critchley <LennyPhoenixC@gmail.com>
2022-09-23 16:22:22 +0100

    Update bot token loader
    Simplify how the bot token is loaded, and prioritise the environment variable as this is less likely to be set

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,5 @@ export const client = new Client({
 // Start
 import("./handlers/events.js")
 
-// List of token sources
-let token_sources = [
-    config.BotConfig.Token,
-    process.env.TOKEN, // Fallback to environment variable if available
-]
-
-// Get token
-let token = ""
-for (let i = 0; i < token_sources.length; i++) {
-    const source = token_sources[i];
-
-    if (source !== undefined && source !== "") {
-        token = source
-        break
-    }
-}
-
 // Login
-client.login(token)
+client.login(process.env.TOKEN || config.BotConfig.Token)
